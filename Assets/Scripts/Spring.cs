9283d9b725f7d5c038855415f0d423367a23da47
dhghgh189 <dhghgh189@gmail.com>
2024-09-25 17:00:20 +0900

    [Feat] 스프링 오브젝트 구현
    스프링 오브젝트 구현

---
+++ Assets/Scripts/Spring.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spring : MonoBehaviour
+{
+    [SerializeField] float jumpPower;
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Rigidbody2D rb = other.gameObject.GetComponent<Rigidbody2D>();
+            rb?.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+        }
+    }
+}
