2af31b98586c4cbffcb2ab4f58a022e0b57d119d
dhghgh189 <dhghgh189@gmail.com>
2024-09-26 12:32:54 +0900

    [Feat] DataManager 기초 구현
    Player Data 로딩 작성

---
+++ Assets/Scripts/Managers/DataManager.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class DataManager : Singleton<DataManager>
+{
+    [SerializeField] List<PlayerSO> listPlayerSO;
+
+    Dictionary<int, PlayerSO> playerDict = new Dictionary<int, PlayerSO>();
+
+    protected override void Init()
+    {
+        LoadData();
+    }
+
+    void LoadData()
+    {
+        LoadPlayerData();
+
+        Debug.Log("Data Load Complete");
+    }
+
+    void LoadPlayerData()
+    {
+        PlayerSO playerData;
+        for (int i = 0; i < listPlayerSO.Count; i++)
+        {
+            playerData = listPlayerSO[i];
+            playerDict.Add(playerData.ID, playerData);
+        }
+    }
+}
